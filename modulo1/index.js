6770a07dce409dd1d1b88c07df36a24261026cc0
eduqg <eduardoqgomes@gmail.com>
2019-10-14 18:04:32 -0300

    Complete CRUD

--- modulo1/index.js
+++ modulo1/index.js
@@ -2,15 +2,42 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 // Query params = ?teste=1
 // Route params = /users/1
 // Request body = {"name": "Eduardo", "email": "some@email.com"}7
 
 const users = ['Eduardo', 'Diego', 'Vitor'];
 
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
+
 server.get('/users/:index', (req, res) => {
   const { index } = req.params;
   return res.json(users[index]);
 });
+
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+  users.push(name);
+  return res.json(users);
+});
+
+server.put('/users/:index', (req, res) => {
+  const { name } = req.body;
+  const { index } = req.params;
+
+  users[index] = name;
+  return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+  // Deleta uma posição
+  const { index } = req.params;
+  users.splice(index, 1);
+  return res.send();
+})
 
 server.listen(3000);
