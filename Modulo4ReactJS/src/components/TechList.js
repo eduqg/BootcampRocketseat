001593d47d58948005e1e0ff054d08ca78374d54
eduqg <eduardoqgomes@gmail.com>
2019-10-22 17:53:57 -0300

    Lifecycle methods

--- Modulo4ReactJS/src/components/TechList.js
+++ Modulo4ReactJS/src/components/TechList.js
@@ -11,12 +11,31 @@ export default class TechList extends Component {
 
   state = {
     newTech: '',
-    techs: [
-      'Node.js',
-      'ReactJS',
-      'React Native'
-    ]
+    techs: []
   };
+
+  // Executado assim que o componente aparece em tela
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+    if (techs) {
+      // Caso atualize página, continuam as techs
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estado
+  // posso colocar _ no lugar dos argumentos se não for usar. prevProps
+  componentDidUpdate(_, prevState) {
+    // this.props ou this.state
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs));
+    }
+  }
+
+  // Executado quando o componente deixa de existir
+  componentWillUnmount() {
+    // As vezes é necessário para remover um event listener (como mouse over)
+  }
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
