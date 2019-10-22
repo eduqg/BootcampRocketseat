9cb42b41b7bd9efd3d748180878738558967f2a0
eduqg <eduardoqgomes@gmail.com>
2019-10-22 17:04:54 -0300

    Immutable state in one component

---
+++ Modulo4ReactJS/src/components/TechList.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+export default class TechList extends Component {
+  state = {
+    newTech: '',
+    techs: [
+      'Node.js',
+      'ReactJS',
+      'React Native'
+    ]
+  };
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    // Array deve ser criado novamente, não pode adicionar elementos
+    this.setState({ techs: [...this.state.techs, this.state.newTech] });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {
+            this.state.techs.map(tech => <li key={tech}>{tech}</li>)
+          }
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    );
+  }
+}
