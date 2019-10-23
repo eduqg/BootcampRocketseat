023b4fbe7f5b4ea1f1dfa0da09dd52ffdf130d76
eduqg <eduardoqgomes@gmail.com>
2019-10-23 20:56:41 -0300

    Main styles with styled components

--- modulo5reactjs/src/pages/Main/index.js
+++ modulo5reactjs/src/pages/Main/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
   return (
-    <Title error={false}>
-      Main
-      <small>menor</small>
-    </Title>
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+      <Form onSubmit={() => { }}>
+        <input
+          type="text"
+          className="text"
+          placeholder="Adicionar repositório"
+        />
+        <SubmitButton disabled>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
