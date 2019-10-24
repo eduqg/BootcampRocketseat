c142ea7f794f1aa812fc59e8cca77959749dd5d5
eduqg <eduardoqgomes@gmail.com>
2019-10-24 09:31:39 -0300

    Styling issues

--- modulo5reactjs/src/pages/Repository/index.js
+++ modulo5reactjs/src/pages/Repository/index.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 import PropTypes from 'prop-types';
 import api from '../../services/api';
+
+import Container from '../../components/Container';
+
+import { Loading, Owner, IssueList } from './styles';
 
 export default class Repository extends Component {
   static propTypes = {
@@ -8,8 +14,8 @@ export default class Repository extends Component {
       params: PropTypes.shape({
         repository: PropTypes.string
       })
-    }).isRequired.
-  }
+    }).isRequired
+  };
 
   state = {
     repository: {},
@@ -29,8 +35,8 @@ export default class Repository extends Component {
         params: {
           state: 'open',
           per_page: 5,
-        }
-      })
+        },
+      }),
     ]);
 
     this.setState({
@@ -42,8 +48,34 @@ export default class Repository extends Component {
 
   render() {
     const { repository, issues, loading } = this.state;
+
+    if (loading) {
+      return <Loading>Carregando...</Loading>
+    }
+
     return (
-      <h1>Repository: </h1>
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos respositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+
+        <IssueList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
+      </Container>
     );
   }
 }
