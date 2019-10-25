a2db265fa5026cc96fa1140a21bbbf8293336bfa
eduqg <eduardoqgomes@gmail.com>
2019-10-25 18:20:41 -0300

    Loading icon after add user

--- modulo6reactnative/src/pages/Main/index.js
+++ modulo6reactnative/src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Keyboard} from 'react-native';
+import { Keyboard, ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
@@ -21,11 +21,15 @@ export default class Main extends Component {
   state = {
     newUser: '',
     users: [],
+    loading: false,
   };
 
   handleAddUser = async () => {
+    // Não precisa do await, animação muito rápida
+    await this.setState({ loading: true });
     const { newUser, users } = this.state;
     const response = await api.get(`/users/${newUser}`);
+
 
     const data = {
       name: response.data.name,
@@ -37,13 +41,14 @@ export default class Main extends Component {
     this.setState({
       users: [...users, data],
       newUser: '',
+      loading: false,
     });
 
     Keyboard.dismiss();
   };
 
   render() {
-    const { users, newUser } = this.state;
+    const { users, newUser, loading } = this.state;
     return (
       <Container>
         <Form>
@@ -56,21 +61,25 @@ export default class Main extends Component {
             returnKeyType="send"
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name="chevron-right" size={20} color="#fff" />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+                <Icon name="add" size={20} color="#fff" />
+              )}
           </SubmitButton>
         </Form>
 
         <List
           data={users}
           keyExtractor={user => user.login}
-          renderItem={({item}) => (
+          renderItem={({ item }) => (
             <User>
-              <Avatar source={{uri: item.avatar}} />
+              <Avatar source={{ uri: item.avatar }} />
               <Name>{item.name}</Name>
               <Bio>{item.bio}</Bio>
 
-              <ProfileButton onPress={() => {}}>
+              <ProfileButton onPress={() => { }}>
                 <ProfileButtonText>Ver Perfil</ProfileButtonText>
               </ProfileButton>
             </User>
