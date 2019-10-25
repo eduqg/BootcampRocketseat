7b78ea150e629268fdbd844b12a44484e5283ca5
eduqg <eduardoqgomes@gmail.com>
2019-10-25 15:39:27 -0300

    Navigation

--- modulo6reactnative/src/index.js
+++ modulo6reactnative/src/index.js
@@ -1,31 +1,8 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
 
 import './config/ReactotronConfig';
-
-const styles = StyleSheet.create({
-  body: {
-    backgroundColor: '#ff00ff',
-  },
-  footer: {
-    color: '#000',
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-console.tron.log('Nois');
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <>
-      <View style={styles.body}>
-        <Text style={styles.footer}>Engine: Hermes</Text>
-        <Text style={styles.footer}>Engine: Hermes</Text>
-      </View>
-    </>
-  );
+  return <Routes />;
 }
