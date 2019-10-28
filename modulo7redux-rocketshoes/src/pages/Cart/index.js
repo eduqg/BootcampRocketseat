70f48f6707a0932bd1221a2ac520b793c8f1126d
eduqg <eduardoqgomes@gmail.com>
2019-10-28 18:23:10 -0300

    Styles to cart

--- modulo7redux-rocketshoes/src/pages/Cart/index.js
+++ modulo7redux-rocketshoes/src/pages/Cart/index.js
@@ -1,9 +1,68 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
   return (
-    <h1>Cart</h1>
+    <Container>
+      <ProductTable>
+        {/* Cabeçalho da tabela */}
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QUANTIDADE</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+                alt="Tenis img"
+              />
+            </td>
+            <td>
+              <strong>Tênis massa</strong>
+              <span>R$130,00</span>
+            </td>
+            <td>
+              {/* Div para poder incluir display flex. td não permite */}
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$260,00</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1920,92</strong>
+        </Total>
+      </footer>
+    </Container>
   );
 }
