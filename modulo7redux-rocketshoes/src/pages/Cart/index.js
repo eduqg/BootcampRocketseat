40df61fe79eae1f4aba0bf56349d74907f3a6ed1
eduqg <eduardoqgomes@gmail.com>
2019-10-29 09:37:35 -0300

    Removing from cart

--- modulo7redux-rocketshoes/src/pages/Cart/index.js
+++ modulo7redux-rocketshoes/src/pages/Cart/index.js
@@ -9,7 +9,7 @@ import {
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart }) {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -51,7 +51,12 @@ function Cart({ cart }) {
                 <strong>R$260,00</strong>
               </td>
               <td>
-                <button type="button">
+                <button
+                  type="button"
+                  onClick={() =>
+                    dispatch({ type: 'REMOVE_FROM_CART', id: product.id })
+                  }
+                >
                   <MdDelete size={20} color="#7159c1" />
                 </button>
               </td>
