48dc85af3ba9e766e32f910b032b31914584cca0
eduqg <eduardoqgomes@gmail.com>
2019-11-03 21:41:23 -0300

    Stock check

--- desafio7reactnative/src/store/modules/cart/sagas.js
+++ desafio7reactnative/src/store/modules/cart/sagas.js
@@ -1,15 +1,48 @@
 import { call, select, put, all, takeLatest } from 'redux-saga/effects';
-
+import { ToastAndroid } from 'react-native';
 import api from '../../../services/api';
 // import history from '../../../services/history';
 
-import { addToCartSuccess, updateAmountSuccess } from './actions';
+import { addToCartSuccess, updateAmount } from './actions';
 
 function* addToCart({ id }) {
-  const response = yield call(api.get, `/products/${id}`);
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
 
-  // Dispara ação
-  yield put(addToCartSuccess(response.data));
+  // Verificação de estoque
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    ToastAndroid.showWithGravityAndOffset(
+      'Quantidade fora de estoque.',
+      ToastAndroid.SHORT,
+      ToastAndroid.BOTTOM,
+      25,
+      50
+    );
+    return;
+  }
+  if (productExists) {
+    // Para produtos repetidos
+    yield put(updateAmount(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      price: response.data.price,
+    };
+    // Dispara ação
+    yield put(addToCartSuccess(data));
+  }
 }
 
 // Quando ADD_REQUEST for disparado, irá chamar addToCart
