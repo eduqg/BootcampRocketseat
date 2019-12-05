06808c805989c4b182e60c4b3384d7afcfc97a4b
eduqg <eduardoqgomes@gmail.com>
2019-12-05 19:00:55 -0300

    Animation to all sides, loop with delay

--- lives/animacoesnative/App.js
+++ lives/animacoesnative/App.js
@@ -25,18 +25,53 @@ const styles = StyleSheet.create({
 export default function App() {
   const [ballY] = useState(new Animated.Value(0));
   // Posso usar add, multiplay, subtract, modulo também
-  const [ballX] = useState(Animated.multiply(ballY, 0.3));
+  const [ballX] = useState(new Animated.Value(0));
 
   // timing = animação linear. duration: 3000,
   // sprint = mesmo que linear, mas efeito elastico. Ao final ele quica. bounciness: 20,
   // decay = joga objeto para cima ou para beixo.
   useEffect(() => {
-    Animated.decay(ballY, {
-      velocity: 0.5,
-      // toValue: 500,
-      // duration: 3000,
-      // bounciness: 20,
-    }).start();
+    Animated.loop(
+      // sequence = executa em sequencia
+      // parallel = executa em paralelo
+      // stagger = espera 100 milisegundos, executa então a segunda animação. Animated.stagger(200, [animações])
+      Animated.sequence([
+        Animated.timing(ballY, {
+          toValue: 200,
+          duration: 500,
+          // velocity: 0.5,
+          // bounciness: 20,
+        }),
+
+        Animated.delay(200),
+
+        Animated.timing(ballX, {
+          toValue: 200,
+          duration: 500,
+        }),
+
+        Animated.delay(200),
+
+        Animated.timing(ballY, {
+          toValue: 0,
+          duration: 500,
+          // velocity: 0.5,
+          // bounciness: 20,
+        }),
+
+        Animated.delay(200),
+
+        Animated.timing(ballX, {
+          toValue: 0,
+          duration: 500,
+        }),
+
+        Animated.delay(200),
+      ]),
+      {
+        iterations: 2,
+      }
+    ).start();
   }, []); // eslint-disable-line
 
   return (
